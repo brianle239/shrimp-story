2255346c9581e3bd640744d76643166834487083
Brian Le <brianle239@gmail.com>
2025-03-23 22:01:50 -0700

    Updates

--- src/models/GameSaveData.ts
+++
@@ -1,9 +0,0 @@
-import { SaveData as PiviVNSaveData } from "@drincs/pixi-vn";
-
-export default interface GameSaveData {
-    saveData: PiviVNSaveData;
-    gameVersion: string;
-    date: Date;
-    name: string;
-    image?: string;
-}
